a75b72b50722e36beb5be6eb39f637f3b237f4f9
MuskanJindal25 <50668760+MuskanJindal25@users.noreply.github.com>
2022-08-17 15:08:40 +0530

    Update 31. Next Permutation.cpp

--- 31. Next Permutation.cpp
+++ 31. Next Permutation.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-void reverse(vector<int> &p, int index){
+void reverse(vector<int> &p, int index){     //Function to reverse vector from given index to last of array
     
     int front = index +1;
     int back = p.size() - 1;
@@ -23,19 +23,19 @@ vector<int> nextPermutation(vector<int> &p, int n)
     
     for(int i=n-1;i>0;i--){
         
-        if(p[i-1]<p[i] && temp == 0){
+        if(p[i-1]<p[i] && temp == 0){    //To find the breaking point - meaning where an ele just less than prev one is present
             temp = p[i-1];
             index = i-1;
-            i=n-1;
+            i=n-1;  // to reset the for loop
         }
         
         if(temp!=0 && p[i]>temp){
-            swap(p[index],p[i]);
+            swap(p[index],p[i]);       //swap the ele found with the ele just larger than it (present on right side)
             reverse(p,index);
             return p;
         }
         
-        if(temp == 0 && i==1){
+        if(temp == 0 && i==1){      //in case of descending order vector - when no next larger permutation is present
             reverse(p,-1);
             return p;
         }
